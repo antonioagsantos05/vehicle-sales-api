9701c7540df758053394c6a4aa93e0552a25f1f2
Antonio Santos <aagomes0505@gmail.com>
2026-07-23 15:13:01 -0300

    Update VehicleSalesApplicationTests.java

--- src/test/java/br/com/fiap/vehiclesales/VehicleSalesApplicationTests.java
+++ src/test/java/br/com/fiap/vehiclesales/VehicleSalesApplicationTests.java
@@ -1,3 +1,17 @@
 package br.com.fiap.vehiclesales;
-import org.junit.jupiter.api.Test; import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest class VehicleSalesApplicationTests { @Test void contextLoads(){} }
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+@SpringBootTest
+class VehicleSalesApplicationTests {
+
+    @MockBean
+    JwtDecoder jwtDecoder;
+
+    @Test
+    void contextLoads() {
+    }
+}
